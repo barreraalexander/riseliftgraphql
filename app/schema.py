6fda5867b66c754d06af12df11d9f9c5c0274938
Alexander Barrera <barreraalexander93@gmail.com>
2024-09-02 18:10:29 -0400

    update

--- app/schema.py
+++ app/schema.py
@@ -10,10 +10,46 @@ class Department(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
+class DepartmentGQL(graphene.ObjectType):
+    name = graphene.String()
+    id = graphene.Int()
+
+class CreateDepartment(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+
+
+    Output = DepartmentGQL
+
+
+    def mutate(root, info, name):
+
+        department_dict = {
+            "name" : name
+        }
+
+        new_department = DepartmentModel(**department_dict)
+
+        db_session.add(new_department)
+        db_session.commit()
+        db_session.refresh(new_department)
+        db_session.close()
+        
+        return new_department
+
+
+
+
+class MyMutations(graphene.ObjectType):
+    create_department = CreateDepartment.Field()
+
 class Employee(SQLAlchemyObjectType):
     class Meta:
         model = EmployeeModel
         interfaces = (relay.Node, )
+
+    # class Arguments:
+    #     name = graphene.String()
 
 
 class Query(graphene.ObjectType):
@@ -23,4 +59,6 @@ class Query(graphene.ObjectType):
     # Disable sorting over this field
     all_departments = SQLAlchemyConnectionField(Department.connection, sort=None)
 
-schema = graphene.Schema(query=Query)
+
+
+schema = graphene.Schema(query=Query, mutation=MyMutations)
